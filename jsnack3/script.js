d7eb8793ce4910b738707fa9715371cd5afc94c9
RodrigoFantechi <rodrifante595@libero.it>
2022-10-12 12:24:24 +0200

    finish jsnack3

---
+++ jsnack3/script.js
@@ -0,0 +1,9 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.  */
+
+let somma = 0;
+for (let i = 0; i < 10; i++) {
+    const userNumber = Number(prompt('Inserisci un numero:'));
+    somma += userNumber;
+}
+console.log(somma);
