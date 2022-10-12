35f38edbd056a9e2f8b5fc67fe2f87668f501a5e
RodrigoFantechi <rodrifante595@libero.it>
2022-10-12 12:11:07 +0200

    add jsnack2 as dom elements

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,13 +3,17 @@ Il software stampa prima la parola più corta, poi la parola più lunga. */
 
 const firstWord = prompt('Inserisci la prima parola:');
 const secondWord  = prompt('Inserisci la seconda parola:');
+const body = document.querySelector('body')
 
 if (firstWord.length>secondWord.length){
     console.log(firstWord);
     console.log(secondWord);
+    body.innerHTML = firstWord +' '+ secondWord;
 }else if (firstWord.length<secondWord.length){
     console.log(secondWord);
     console.log(firstWord);
+    body.innerHTML = secondWord +' '+ firstWord;
 } else{
     console.log('le due parole sono lunghe uguali');
+    body.innerHTML = 'le due parole sono lunghe uguali';
 }
