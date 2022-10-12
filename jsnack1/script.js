b94b066d6e453b3e3cca31f04fc58caf22e82316
RodrigoFantechi <rodrifante595@libero.it>
2022-10-12 11:37:57 +0200

    print as dom element

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,10 +3,12 @@ Il software stampa il maggiore. */
 
 const numberOne = Number(prompt('Inserisci il primo numero:'));
 const numberTwo = Number(prompt('Inserisci il secondo numero:'));
-
+const body = document.querySelector('body')
 
 if (numberOne > numberTwo){
     console.log(numberOne);
+    body.innerHTML = numberOne;
 }else{
     console.log(numberTwo);
+    body.innerHTML = numberTwo;
 }
