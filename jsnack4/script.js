e2aca62e89de143ecb27c2fa43e73cb17fd9229f
RodrigoFantechi <rodrifante595@libero.it>
2022-10-13 12:38:03 +0200

    fix exercise

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,6 +1,6 @@
 /* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. */
 
-const name =[
+const ospiti =[
     'Giovanni',
     'Giorgio',
     'Genoveffo',
@@ -8,13 +8,28 @@ const name =[
 ];
 
 const userName = prompt('mi fornisca gentilmente il suo nome Bonuomo');
+let valid = 'non può entrare villano';
 
-let valid = 'non posso entrare villano';
-
-for (let i = 0; i < name.length; i++) {
-    const element = name[i];
+for (let i = 0; i < ospiti.length; i++) {
+    const element = ospiti[i];
     if (element===userName){
         valid = 'lei è il benvenuto Messere';
     }
+} 
+
+// OPPURE 
+    let i = 0;
+    while ( i < ospiti.length) {
+        const element = ospiti[i];
+        if (element===userName){
+            valid = 'lei è il benvenuto Messere';
+        }
+        i++
 }
+// OPPURE 
+
+if (ospiti.includes(userName)){
+    valid = 'lei è il benvenuto Messere';
+}
+
 alert(valid);
