86035975a229e4f4f28282cd23bd3439c366648c
RodrigoFantechi <rodrifante595@libero.it>
2022-10-13 11:59:52 +0200

    add ex with while

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,11 +1,29 @@
 /* Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.  */
 
+// const body = document.querySelector('body')
+// let somma = 0;
+// for (let i = 0; i < 10; i++) {
+//     const userNumber = Number(prompt('Inserisci un numero:'));
+//     somma += userNumber;
+// }
+// console.log(somma);
+// body.innerHTML = somma;
+
+
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.  con ciclo For */
+
+
 const body = document.querySelector('body')
 let somma = 0;
-for (let i = 0; i < 10; i++) {
+let i = 0
+while (i < 10) {
     const userNumber = Number(prompt('Inserisci un numero:'));
     somma += userNumber;
+    i++;
 }
 console.log(somma);
 body.innerHTML = somma;
+
+
